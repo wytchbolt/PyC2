a323fb1666dce75eb635e72198f4b0885f90f89c
Célestine <78518052+wytchbolt@users.noreply.github.com>
2023-07-19 20:55:52 -0500

    Create PyC2.py

---
+++ PyC2.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import os
+
+directory = r'DIRECTORY_PATH'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".png"):
+        img = Image.open(filename)
+        name = (filename)
+        cropped = img.crop((0,90,2880,1710))
+        cropped.save(name)
+        print(os.path.join(directory, filename))
+        continue
+    else:
+        continue
+for filename in os.listdir(directory):
+    if filename.endswith(".png"):
+        img = Image.open(filename)
+        img = img.convert("RGB")
+        img.save(filename, "JPEG", optimize=True, quality=80)
+        print(os.path.join(directory, filename))
+        continue
+    else:
+        continue
