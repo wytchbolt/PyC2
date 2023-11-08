4ee9da769d9c9ae93ac9e62a2b6b5eae4e74d499
Célestine <78518052+wytchbolt@users.noreply.github.com>
2023-11-08 10:35:56 -0600

    Update PyC2.py

--- PyC2.py
+++ PyC2.py
@@ -2,12 +2,15 @@ from PIL import Image
 import os
 
 directory = r'DIRECTORY_PATH'
+# Change 'DIRECTORY_PATH' to the path of the folder with uncropped images and PyC2.py
 
 for filename in os.listdir(directory):
     if filename.endswith(".png"):
         img = Image.open(filename)
         name = (filename)
         cropped = img.crop((L,T,R,B))
+        # Chance 'L,T,R,B' to crop dimensions
+        # EG '0,90,2880,1710'
         cropped.save(name)
         print(os.path.join(directory, filename))
         continue
@@ -18,6 +21,7 @@ for filename in os.listdir(directory):
         img = Image.open(filename)
         img = img.convert("RGB")
         img.save(filename, "JPEG", optimize=True, quality=80)
+        # Quality is set to 80 by default
         print(os.path.join(directory, filename))
         continue
     else:
